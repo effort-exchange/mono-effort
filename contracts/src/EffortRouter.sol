c335d28bb487258f740ba0b1b07ebb8f384fc6b4
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-12-16 21:53:23 +0700

    chore: add testing framework

--- contracts/src/EffortRouter.sol
+++ contracts/src/EffortRouter.sol
@@ -5,9 +5,24 @@ import {Checkpoints} from "@openzeppelin/contracts/utils/structs/Checkpoints.sol
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 import {EffortBase} from "./EffortBase.sol";
+import {IEffortRegistry} from "./interface/IEffortRegistry.sol";
+import {IEffortRouter} from "./interface/IEffortRouter.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
-contract EffortRouter is EffortBase {
-    constructor() {
+contract EffortRouter is EffortBase, IEffortRouter, ReentrancyGuardUpgradeable {
+    IEffortRegistry immutable REGISTRY;
+
+    constructor(IEffortRegistry registry) {
+        REGISTRY = registry;
         _disableInitializers();
+    }
+
+    function initializeAll(address initialOwner) public {
+        EffortBase.initialize(initialOwner);
+        initialize2();
+    }
+
+    function initialize2() public reinitializer(2) {
+        __ReentrancyGuard_init();
     }
 }
---
+++ contracts/test/TestSuite.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.24;
+
+import {MockERC20} from "./MockERC20.sol";
+import {EffortBase} from "../src/EffortBase.sol";
+import {EffortRegistry} from "../src/EffortRegistry.sol";
+import {EffortRouter} from "../src/EffortRouter.sol";
+import {EffortVaultFactory} from "../src/EffortVaultFactory.sol";
+import {EffortVault} from "../src/EffortVault.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {UnsafeUpgrades} from "@openzeppelin/foundry-upgrades/Upgrades.sol";
+
+/**
+ * @dev This test suite set up all the  contracts needed for testing.
+ */
+contract TestSuite is Test {
+    address public owner = vm.randomAddress();
+
+    address public baseImpl = address(new EffortBase());
+
+    EffortRouter public router;
+    EffortRegistry public registry;
+    EffortVaultFactory public vaultFactory;
+
+    function setUp() public virtual {
+        bytes memory baseInit = abi.encodeCall(EffortBase.initialize, (owner));
+
+        router = EffortRouter(UnsafeUpgrades.deployUUPSProxy(baseImpl, baseInit));
+        registry = EffortRegistry(UnsafeUpgrades.deployUUPSProxy(baseImpl, baseInit));
+        vaultFactory = EffortVaultFactory(UnsafeUpgrades.deployUUPSProxy(baseImpl, baseInit));
+
+        EffortVault vaultImpl = new EffortVault(router, registry);
+        address beacon = UnsafeUpgrades.deployBeacon(address(vaultImpl), owner);
+
+        vm.startPrank(owner);
+        UnsafeUpgrades.upgradeProxy(
+            address(router), address(new EffortRouter(registry)), abi.encodeCall(EffortRouter.initialize2, ())
+        );
+        UnsafeUpgrades.upgradeProxy(
+            address(registry), address(new EffortRegistry(router)), abi.encodeCall(EffortRegistry.initialize2, ())
+        );
+        UnsafeUpgrades.upgradeProxy(address(vaultFactory), address(new EffortVaultFactory(beacon, registry)), "");
+        vm.stopPrank();
+    }
+
+    function _advanceBlockBy(uint256 blocks) internal {
+        vm.roll(block.number + blocks);
+        vm.warp(block.timestamp + (12 * blocks));
+    }
+
+    function _advanceBlockBySeconds(uint256 newSeconds) internal {
+        vm.roll(block.number + (newSeconds / 12));
+        vm.warp(block.timestamp + newSeconds);
+    }
+}
