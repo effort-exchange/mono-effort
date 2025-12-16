499fc63da962d5dfbdfc4cfca26a1108eac71b7b
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2025-12-16 21:54:25 +0700

    chore: add testing framework (#6)

--- contracts/src/EffortRegistry.sol
+++ contracts/src/EffortRegistry.sol
@@ -5,9 +5,29 @@ import {Checkpoints} from "@openzeppelin/contracts/utils/structs/Checkpoints.sol
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 import {EffortBase} from "./EffortBase.sol";
+import {IEffortRegistry} from "./interface/IEffortRegistry.sol";
+import {IEffortRouter} from "./interface/IEffortRouter.sol";
 
-contract EffortRegistry is EffortBase {
-    constructor() {
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+contract EffortRegistry is EffortBase, IEffortRegistry, ReentrancyGuardUpgradeable {
+    IEffortRouter immutable ROUTER;
+
+    constructor(IEffortRouter router) {
+        ROUTER = router;
         _disableInitializers();
+    }
+
+    function initializeAll(address initialOwner) public {
+        EffortBase.initialize(initialOwner);
+        initialize2();
+    }
+    
+    function initialize2() public reinitializer(2) {
+        __ReentrancyGuard_init();
+    }
+
+    function isOperator(address account) external view returns(bool){
+        return 1 == 2;
     }
 }
